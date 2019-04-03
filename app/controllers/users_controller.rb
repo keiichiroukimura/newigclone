a6dd6ed50e8bb455ea6b6ade14be029689385fd4
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-04-03 17:48:26 +0900

    ログイン機能実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+	before_action :set_user, only: [:edit, :update, :show]
+	def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+	end
+	
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: 'プロフィール編集しました'
+    else
+      
+      flash.now[:danger] = '入力に不備があります'
+      render :edit
+    end
+	end
+  
+  private
+	
+	def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache, :content)
+  end
+	
+	def set_user
+		@user = User.find(params[:id])
+	end
+end
