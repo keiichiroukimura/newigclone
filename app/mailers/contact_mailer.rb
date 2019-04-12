ef92eb9185a061c703fe7ad9ee60aaff8df6f0fc
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-04-12 16:03:56 +0900

    投稿確認メール内容変更

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: "kkhtwdect@gmail.com", subject: "お問い合わせの確認メール"
+    mail to: "kkhtwdect@gmail.com", subject: "画像投稿の確認メール"
   end
 end
