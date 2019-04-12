7a6b7af9c7b78b5a87b3c86902a0a8dcf700f787
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-04-13 07:00:18 +0900

    インデント修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,63 +1,63 @@
 class PostsController < ApplicationController
-	before_action :set_post, only: [:show, :edit, :update, :destroy ]
+  before_action :set_post, only: [:show, :edit, :update, :destroy ]
 	
-	def index
-		@posts = Post.all.order(id: "DESC") 
-	end
+  def index
+    @posts = Post.all.order(id: "DESC") 
+  end
 
-	def new
-		if params[:back]
-			@post = Post.new(post_params)
-		else
-			@post = Post.new
-		end
-	end
+  def new
+    if params[:back]
+      @post = Post.new(post_params)
+    else
+      @post = Post.new
+    end
+  end
 	
-	def show
-		@favorite = current_user.favorites.find_by(post_id: @post.id)
-	end	
+  def show
+    @favorite = current_user.favorites.find_by(post_id: @post.id)
+  end	
 	
-	def confirm
-		@post = Post.new(post_params)
+  def confirm
+    @post = Post.new(post_params)
     @post.user_id = current_user.id
-		render :new if @post.invalid?
-	end
+    render :new if @post.invalid?
+  end
 
-	def edit;end
+  def edit;end
 	
-	def create
-		@post = Post.new(post_params)
+  def create
+    @post = Post.new(post_params)
     @post.user_id = current_user.id
-		if @post.save
-			flash[:success] = "新規投稿。"
-			redirect_to posts_path
-		else
-			render 'new'
-		end
-	end
+    if @post.save
+      flash[:success] = "新規投稿。"
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
 	
-	def update
-		@post.user_id == current_user.id
-		if @post.update(post_params)
-			redirect_to posts_path
-		else
-			render 'edit'
-		end
-	end 
+  def update
+    @post.user_id == current_user.id
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end 
 
-	def destroy
-		@post.destroy
+  def destroy
+    @post.destroy
     flash[:danger] = "投稿を削除しました。"
     redirect_to posts_path
-	end
+  end
 		
-	private
+  private
 	
-	def set_post
+  def set_post
     @post = Post.find(params[:id])
   end
 	
-	def post_params
-		params.require(:post).permit(:image, :image_cache, :content, :user_id)
-	end
+  def post_params
+    params.require(:post).permit(:image, :image_cache, :content, :user_id)
+  end
 end
