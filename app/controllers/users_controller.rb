6c6bdcb381940e21a5111d4b3c0bfac10b224b88
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-04-13 06:12:56 +0900

    インデント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:edit, :update, :show]
-  skip_before_action :login_required, only: [:new, :create]
-  
+before_action :set_user, only: [:edit, :update, :show]
+skip_before_action :login_required, only: [:new, :create]
+
   def new
     @user = User.new
   end
   
   def create
-    @user = User.new(user_params)
+      @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to user_path(@user.id)
@@ -17,24 +17,24 @@ class UsersController < ApplicationController
   end
   
   def show;end
-  
+
   def edit;end
-	
+
   def update
     if @user.update(user_params)
       redirect_to user_path(@user.id), notice: 'プロフィール編集しました'
     else
       flash.now[:danger] = '入力に不備があります'
-      render :edit
+      render 'edit'
     end
-	end
+  end
   
   private
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache, :content, :image_name)
   end
-	
+  
   def set_user
     @user = User.find(params[:id])
   end
