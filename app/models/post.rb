9499042e47d338571e2208a1db1ec52cf358a649
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-04-13 09:56:16 +0900

    ルート変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  as_many :favorites, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   mount_uploader :image, ImageUploader
   validates :image,:content, presence: true
