9a24d3ca46638c5654cf9bada74375d0041436b0
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-04-15 08:30:04 +0900

    contactMVC削除

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 20190409204827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "contacts", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
